726560326c6bf163a54ba64dd7a253d50db0b848
Maria Kushlianskaya <mariakazakova92@gmail.com>
2021-09-01 17:53:52 +0300

    append flags in divs

--- src/scripts/getCompetitorInfo.js
+++ src/scripts/getCompetitorInfo.js
@@ -1,7 +1,3 @@
-//Объект
-//запрос => рандомный объект
-//из объекта получить страну => получить флаг
-
 export function getCompetitorInfo() {
 
     let generalInfo = fetch("https://randomuser.me/api/")
@@ -12,15 +8,80 @@ export function getCompetitorInfo() {
     return generalInfo;
 }
 
-getCompetitorInfo().then((nat) => {
-    console.log("nat", nat);
+let divForFlag = document.querySelectorAll(".competitor-info__flag");
+divForFlag = Array.from(divForFlag);
 
-    let imgFlag = document.createElement("img");
-    imgFlag.src = `https://www.countryflags.io/${nat}/flat/64.png`;
+let divForBetWindow = document.querySelectorAll(".bet-window__flag");
+divForBetWindow = Array.from(divForBetWindow);
 
-    let divForFlag = document.querySelector(".flag");
-    divForFlag.append(imgFlag);
+function imgFlag() {
+    let imgFlag = getCompetitorInfo().then((nat) => {
+
+        let imgFlag = document.createElement("img");
+        imgFlag.src = `https://www.countryflags.io/${nat}/flat/64.png`;
+
+        return imgFlag;
+
+    })
+        .catch(console.log);
+    return imgFlag;
+
+}
 
 
-})
-    .catch(console.log);
+divForFlag.map((item, i, arr) => {
+    imgFlag().then((imgFlag) => {
+        item.append(imgFlag);
+
+        let newFlag = new Image;
+        newFlag.src = imgFlag.src;
+        divForBetWindow[i].append(newFlag)
+
+    })
+        .catch(console.log);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// классы
+
+// class Competitor {
+//     constructor(nat) {
+//         this.nat = nat;
+//         this.generalInfo = generalInfo;
+//     }
+//     getCompetitorInfo() {
+
+//         let generalInfo = fetch("https://randomuser.me/api/")
+//             .then(Response => Response.json())
+//             .then(data => data.results[0].nat)
+//             .catch(err => console.log(err))
+
+//         return generalInfo;
+//     }
+
+//     appendFlag() {
+//         getCompetitorInfo().then((nat) => {
+
+//             let imgFlag = document.createElement("img");
+//             imgFlag.src = `https://www.countryflags.io/${nat}/flat/64.png`;
+
+//             let divForFlag = document.querySelector(".flag");
+//             divForFlag.append(imgFlag);
+//         })
+//             .catch(console.log);
+
+//     }
+// }
